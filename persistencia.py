b41706c046644630ebed2e02d92ce4c3c4d9dcf8
begonavaldivieso <begona.valdivieso@getronics.com>
2023-01-02 21:49:38 +0100

    cambios para corregir pylint

--- persistencia.py
+++ persistencia.py
@@ -1,5 +1,7 @@
-# Funcion que guarda en una fila del fichero pedidos.txt los parametros
+"""Funcion que guarda en una fila del fichero pedidos.txt los parametros """
+
 def guardar_pedido(nombre, apellidos):
+    """ Funcion que guarda en una fila del fichero pedidos.txt los parametros"""
     with open("pedidos.txt", "a", encoding="utf-8") as file:
         file.write(nombre + " " + apellidos + "\n")
         file.close()
