0519feff6372341977c32c7495a96bdec522cb43
begonavaldivieso <begona.valdivieso@getronics.com>
2023-01-02 21:11:53 +0100

    test_persistencia_tareaFinal
    Tarea final. Se renombra el otro fichero que teniamos

--- test_persistencia.py
+++ test_persistencia.py
@@ -1,21 +1,17 @@
-# Importamos la función persistencia
+"""
+Pruebas Persistencia
+"""
 import persistencia
 
-# Borramos el contenido del fichero pedidos.txt si lo hubiera.
-with open("pedidos.txt", "w", encoding="utf-8") as file:
-    file.write("")
-    file.close()
-
-# Declaramos la lista
-pedidos = list()
-# Rellenamos la lista con los diccionarios
-pedidos = [
-    {"nombre": "Pedro", "apellidos": "Gil de Diego"},
-    {"nombre": "Michael", "apellidos": "Jordan"}
-]
-
-# Recorremos la lista y llamos a la funcion persistencia
-for pedido in pedidos:
-    nombre = pedido["nombre"]
-    apellidos = pedido["apellidos"]
-    persistencia.guardar_pedido(nombre, apellidos)
+def test_guardar_pedido():
+    """
+    Prueba general
+    """
+    with open("pedidos.txt", "w+", encoding="utf-8") as file:
+      persistencia.guardar_pedido("Pedro", "Gil de Diego")
+      persistencia.guardar_pedido("Michael", "Jordan")
+      firstline = file.readline()
+      secondline = file.readline()
+      file.close()
+    assert firstline == "-Pedro Gil de Diego\n"
+    assert secondline == "-Michael Jordan\n"
