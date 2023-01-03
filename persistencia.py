5d7d32b74ff6b548fa4b3faaac3d19fa60ebe82d
begonavaldivieso <begona.valdivieso@getronics.com>
2023-01-03 17:12:38 +0100

    Update persistencia.py
    Se añade el guion por delante del nombre

--- persistencia.py
+++ persistencia.py
@@ -3,5 +3,5 @@
 def guardar_pedido(nombre, apellidos):
     """ Funcion que guarda en una fila del fichero pedidos.txt los parametros"""
     with open("pedidos.txt", "a", encoding="utf-8") as file:
-        file.write(nombre + " " + apellidos + "\n")
+        file.write("-" + nombre + " " + apellidos + "\n")
         file.close()
